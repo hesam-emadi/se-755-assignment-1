32e138da7a68da062aa132e0a0ee76579f047a45
Hesam Emadi <sema523@aucklanduni.ac.nz>
2018-08-30 15:32:58 +1200

    Added worldcup regression.

--- scratchpad/Tutorial 2 codes/linear_regression_demo.py
+++ scratchpad/Tutorial 2 codes/linear_regression_demo.py
@@ -18,14 +18,12 @@ diabetes = datasets.load_diabetes()
 # Use only one feature
 diabetes_X = diabetes.data[:, [2]]
 
-#one target attribute
-diabetes_y=diabetes.target
+# one target attribute
+diabetes_y = diabetes.target
 
 # Split the data into training/testing sets
-diabetes_X_train, diabetes_X_test,diabetes_y_train,diabetes_y_test= \
-train_test_split(diabetes_X,diabetes_y,test_size=0.2,random_state=1)
-
-
+diabetes_X_train, diabetes_X_test, diabetes_y_train, diabetes_y_test = \
+    train_test_split(diabetes_X, diabetes_y, test_size=0.2, random_state=1)
 
 # Create linear regression object
 regr = linear_model.LinearRegression()
@@ -40,7 +38,7 @@ diabetes_y_train_pred = regr.predict(diabetes_X_train)
 
 print(' ')
 # The coefficients
-print('Coefficients and Intercept are: ', regr.coef_,"   ",regr.intercept_,' respectively')
+print('Coefficients and Intercept are: ', regr.coef_, "   ", regr.intercept_, ' respectively')
 # The mean squared error
 print('_________________###################____________________')
 print("Mean squared error for testing data: %.2f"
@@ -54,29 +52,26 @@ print("Mean squared error for training data: %.2f"
 print('Variance score for training data: %.2f' % r2_score(diabetes_y_train, diabetes_y_train_pred))
 
 # Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
+plt.scatter(diabetes_X_test, diabetes_y_test, color='black')
 plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
 
-plt.xticks(np.arange(start=-0.1,stop=0.2,step=0.06))
-plt.yticks(np.arange(500,step=100))
+plt.xticks(np.arange(start=-0.1, stop=0.2, step=0.06))
+plt.yticks(np.arange(500, step=100))
 
 plt.show()
 
 #######################################################version 2#############################################################
 from mpl_toolkits.mplot3d import Axes3D
 
-
 # Use two features
-diabetes_X = diabetes.data[:, [3,2]]
+diabetes_X = diabetes.data[:, [3, 2]]
 
-#one target attribute
-diabetes_y=diabetes.target
+# one target attribute
+diabetes_y = diabetes.target
 
 # Split the data into training/testing sets
-diabetes_X_train, diabetes_X_test,diabetes_y_train,diabetes_y_test= \
-train_test_split(diabetes_X,diabetes_y,test_size=0.2,random_state=1)
-
-
+diabetes_X_train, diabetes_X_test, diabetes_y_train, diabetes_y_test = \
+    train_test_split(diabetes_X, diabetes_y, test_size=0.2, random_state=1)
 
 # Create linear regression object
 regr = linear_model.LinearRegression()
@@ -88,34 +83,34 @@ regr.fit(diabetes_X_train, diabetes_y_train)
 diabetes_y_pred = regr.predict(diabetes_X_test)
 
 # The coefficients
-print('Coefficients and Intercept are: ', regr.coef_,"   ",regr.intercept_,' respectively')
+print('Coefficients and Intercept are: ', regr.coef_, "   ", regr.intercept_, ' respectively')
 # The mean squared error
 print("Mean squared error: %.2f"
       % mean_squared_error(diabetes_y_test, diabetes_y_pred))
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred))
 
-#Plot outputs
+# Plot outputs
 fig = plt.figure(1, figsize=(9, 6))
 ax = Axes3D(fig)
-#plot testing points
-ax.scatter(diabetes_X_test[:,0], diabetes_X_test[:,1],diabetes_y_test,depthshade=False,c='black')
-
+# plot testing points
+ax.scatter(diabetes_X_test[:, 0], diabetes_X_test[:, 1], diabetes_y_test, depthshade=False, c='black')
 
 x = np.arange(-0.2, 0.2, 0.05)
 N = x.size
-a,b = np.meshgrid(x,x)
-it = np.array([b.ravel(),a.ravel(),np.ones(N*N)]).T
-w=np.append(regr.coef_,regr.intercept_)
-result=it.dot(w)
-#plot fitted hyperplane
-ax.plot_surface(np.reshape(it[:,0],(N,N)), np.reshape(it[:,1],(N,N)), np.reshape(result,(N,N)),shade=False,color=(0.,1.,0.,0.2))
-#set axis 
+a, b = np.meshgrid(x, x)
+it = np.array([b.ravel(), a.ravel(), np.ones(N * N)]).T
+w = np.append(regr.coef_, regr.intercept_)
+result = it.dot(w)
+# plot fitted hyperplane
+ax.plot_surface(np.reshape(it[:, 0], (N, N)), np.reshape(it[:, 1], (N, N)), np.reshape(result, (N, N)), shade=False,
+                color=(0., 1., 0., 0.2))
+# set axis
 ax.set_title("Two features regression")
 ax.set_xlabel("4th feature")
 ax.w_xaxis.set_ticklabels([])
 ax.set_ylabel("3rd feature")
 ax.w_yaxis.set_ticklabels([])
 ax.set_zlabel("target")
-ax.w_zaxis.set_ticklabels(np.arange(500,step=100))
+ax.w_zaxis.set_ticklabels(np.arange(500, step=100))
 plt.show()
---
+++ worldcupfinal-regression.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import Imputer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+import category_encoders as cs
+from sklearn.pipeline import FeatureUnion
+
+from sklearn.linear_model import Ridge
+
+
+def predictLinearRegression(trainingData, testData, trainingTarget, testTarget):
+    # Create linear regression object
+    regr = linear_model.LinearRegression()
+
+    # Train the model using the training sets
+    regr.fit(trainingData, trainingTarget)
+
+    # Make predictions using the testing set
+    testDataPrediction = regr.predict(testData)
+    # Make predictions using the testing set
+    trainingDataPrediction = regr.predict(trainingData)
+
+    print(' ')
+    # The coefficients
+    print('Coefficients and Intercept are: ', regr.coef_, "   ", regr.intercept_, ' respectively')
+    # The mean squared error
+    print('** Linear Regression *******************************************************')
+    print("Mean squared error for testing data: %.2f"
+          % mean_squared_error(testTarget, testDataPrediction))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score for testing data: %.2f' % r2_score(testTarget, testDataPrediction))
+    print('******************************************************* ')
+    print("Mean squared error for training data: %.2f"
+          % mean_squared_error(trainingTarget, trainingDataPrediction))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score for training data: %.2f' % r2_score(trainingTarget, trainingDataPrediction))
+
+
+def predictRidgeRegression(trainingData, testData, trainingTarget, testTarget):
+    # Create linear regression object
+    regr = Ridge(alpha=.5)
+
+    # Train the model using the training sets
+    regr.fit(trainingData, trainingTarget)
+
+    # Make predictions using the testing set
+    testDataPrediction = regr.predict(testData)
+    # Make predictions using the testing set
+    trainingDataPrediction = regr.predict(trainingData)
+
+    print(' ')
+    # The coefficients
+    print('Coefficients and Intercept are: ', regr.coef_, "   ", regr.intercept_, ' respectively')
+    # The mean squared error
+    print('** Ridge Regression *******************************************************')
+    print("Mean squared error for testing data: %.2f"
+          % mean_squared_error(testTarget, testDataPrediction))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score for testing data: %.2f' % r2_score(testTarget, testDataPrediction))
+    print('******************************************************* ')
+    print("Mean squared error for training data: %.2f"
+          % mean_squared_error(trainingTarget, trainingDataPrediction))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score for training data: %.2f' % r2_score(trainingTarget, trainingDataPrediction))
+
+
+worldcup = pd.read_csv("./resource/world-cup-2018/worldcup-2018.csv", index_col=0)
+
+worldcup.drop(['Location', 'Phase', 'Date', 'Team1_Ball_Possession(%)'], axis=1, inplace=True)
+worldcup.describe()
+
+# world cup attributes
+worldcupAllFeatures = worldcup.iloc[:, np.arange(24)].copy()
+# world cup goal result
+scoreAsTarget = worldcup.iloc[:, 24].copy()
+# wordl cup match result
+w_results = worldcup.iloc[:, 25].copy()
+
+
+# Create a class to select numerical or categorical columns
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.attribute_names].values
+
+
+numericFeatures = worldcupAllFeatures.drop(
+    ['Team1', 'Team2', 'Team1_Continent', 'Team2_Continent', 'Normal_Time'], axis=1, inplace=False)
+
+stringFeatures = worldcupAllFeatures[
+    ['Team1', 'Team2', 'Team1_Continent', 'Team2_Continent', 'Normal_Time']].copy()
+
+numericFeaturePipeline = Pipeline([
+    ('selector', DataFrameSelector(list(numericFeatures))),
+    ('imputer', Imputer(strategy="median")),
+    ('std_scaler', StandardScaler()),
+])
+
+stringFeaturePipeline = Pipeline([
+    ('selector', DataFrameSelector(list(stringFeatures))),
+    ('cat_encoder', cs.OneHotEncoder(drop_invariant=True)),
+])
+
+full_pipeline = FeatureUnion(transformer_list=[
+    ("num_pipeline", numericFeaturePipeline),
+    ("cat_pipeline", stringFeaturePipeline),
+])
+
+feature_prepared = pd.DataFrame(data=full_pipeline.fit_transform(worldcupAllFeatures), index=np.arange(1, 65))
+# worldcup_cleaned = pd.concat([feature_prepared, w_goals.to_frame(), w_results.to_frame()], axis=1)
+
+# Split the data into training/testing sets
+worldcupFeatureTrainingData, testData, worldcupTargetTrainingData, testTarget = \
+    train_test_split(feature_prepared, scoreAsTarget, test_size=0.2, random_state=1)
+
+predictLinearRegression(worldcupFeatureTrainingData, testData, worldcupTargetTrainingData, testTarget)
+predictRidgeRegression(worldcupFeatureTrainingData, testData, worldcupTargetTrainingData, testTarget)
